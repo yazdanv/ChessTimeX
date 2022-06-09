dfbb1bf0fdb39c2bc8603aaa3ccb620038311656
Yazdan <ymazdy@gmail.com>
2022-06-09 23:16:04 +0430

    Initial Commit

---
+++ ChessTimeX/ChessTimeXApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChessTimeXApp.swift
+//  ChessTimeX
+//
+//  Created by Yazdan on 6/9/22.
+//
+
+import SwiftUI
+
+@main
+struct ChessTimeXApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
